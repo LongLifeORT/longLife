68d625405398b54ec6971ddace01f6fe931c1e2c
BRUNO-TAB\Bruno <fleischer.bruno@gmail.com>
2017-06-20 22:05:10 -0300

    Se agregan funciones accesorias y se elimina estructura de objeto

--- Medico.js
+++ Medico.js
@@ -1,65 +1,104 @@
-medico: {
-	init: function(){
-		// Inicializa los metodos necesarios para que funcione la aplicación con el login de usuario medico
-		this.domCache();
-		this.readyEvents();
-	},
-	domCache: function(){
-		// Guarda los elementos del DOM que se van a utilizar por el medico
-	},
-	readyEvents: function(){
-		// Genera los event handlers necesarios para el medico (por ejemplo, si es medico y hace click en login, que busque solo en la lista de medico.
-	},
-	listPatients: function(){
-		// Ordena los datos de los pacientes para imprimirlos y que el doc pueda elegir que paciente modificar
-	},
-	showPatient: function(){
-		// Similar a listPatients(), pero muestra solo los datos del paciente seleccionado
-	}
-	updatePatient: function(){
-		// Metodo para agregar nuevo peso, altura, etc. Similar al metodo modifyPatient() del objeto paciente
-	},
-	
-	//Creamos un constructor para el objeto medico
-	//De los doctores se conoce nombre, apellido, su especialidad, numero de profesional.
-	/*Al ser un constructor, su nomenclatura comienza en Mayuscula, lleva de argumentos las propiedades que ya conocemos de los medicos*/
-			
-	create: function Medico(nombre, apellido, especialidad, numeroProfesional,contraseña){
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.especialidad = especialidad;
-		this.numeroProfesional = numeroProfesional;
-		this.contraseña = contraseña;
+function Medico(nombre, apellido, especialidad, numeroProfesional,clave){
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.especialidad = especialidad;
+	this.numeroProfesional = numeroProfesional;
+	this.clave = clave;
 		
-			},
-
-	doctores: [
-	  
-		new Medico("Gerardo", "Torres", "Cardiologo", 123,123456),
-		new Medico("Gabino", "Baldomero", "Geriatra",124 ,123456),
-		new Medico("Modesto", "Abel", "Neumología",   125, 123456),
-		new Medico("Ignacio", "Cogoyo", "Neumología", 126,123456),
-		new Medico("Silvio", "Bautista", "Pediatría", 127,123456),
-		new Medico("Ramiro", "Nicola", "Pediatría", 128,123456),
-		new Medico("Adalberto", "Glauco", "Psiquiatría", 129,123456),
-		new Medico(	"Amado", "Cipriano", "Toxicología", 130,123456),
-		new Medico("Adolfo", "Wilfredo", "Oftalmología", 131,123456),
-		new Medico("Rosario", "Reyes", "Cardiologo", 132,123456),			
-	],
-		
-	init: function(){
-		//inicializa a medico, doctor y todos los metodos necesarios para que la página muestre reportes, dibuje, etc.
-	},
-	domCache: function(){
-			// Guarda los elementos del DOM que no son especificos a medico o paciente
-	},
-	readyEvents: function(){
-		// Genera los event handlers que no son especificos a medico o paciente
-	},
-	generateReport: function(){
-		//retorna un JSON o string de los medicos, sus especialidades, y sus consultas
-	},
-	showReport: function(){
-		// dibuja el reporte generado
-	}
 }
+
+var doctores = [
+	new Medico("Gerardo", "Torres", "Cardiologo", 123,123456),
+	new Medico("Gabino", "Baldomero", "Geriatra",124 ,123456),
+	new Medico("Modesto", "Abel", "Neumología",   125, 123456),
+	new Medico("Ignacio", "Cogoyo", "Neumología", 126,123456),
+	new Medico("Silvio", "Bautista", "Pediatría", 127,123456),
+	new Medico("Ramiro", "Nicola", "Pediatría", 128,123456),
+	new Medico("Adalberto", "Glauco", "Psiquiatría", 129,123456),
+	new Medico(	"Amado", "Cipriano", "Toxicología", 130,123456),
+	new Medico("Adolfo", "Wilfredo", "Oftalmología", 131,123456),
+	new Medico("Rosario", "Reyes", "Cardiologo", 132,123456),			
+];
+
+
+/**
+ arrConsultas array
+ indexMedico int
+ return array
+*/
+function buscarConsultas(arrConsultas, indexMedico){
+	var arrayResultado = [];
+	for(var i = 0; i < arrConsultas.length; i++){
+		if(arrConsultas[i].medico === indexMedico){
+			arrayResultado.push(arrConsultas[i]);
+		}
+	}
+	return arrayResultado;
+}
+
+/**
+ modificarPesoAltura
+ peso int
+ altura float
+ pacienteIndex int
+
+ return undefined
+*/
+function modificarPesoAltura(peso, altura, pacienteIndex){
+	paciente[pacienteIndex].peso.push(peso);
+	paciente[pacienteIndex].altura.push(altura);
+}
+
+/**
+ pacienteIndex int
+ 
+ returns bool
+*/
+function variacionMayorADiezIMC(pacienteIndex){
+	var pesoAnterior = paciente[pacienteIndex].peso[peso.length - 2];
+	var alturaAnterior = paciente[pacienteIndex].altura[altura.length - 2];
+	var pesoNuevo = paciente[pacienteIndex].peso[peso.length - 1];
+	var alturaNuevo = paciente[pacienteIndex].altura[altura.length - 2];
+	var imcViejo = calcularIMC(pesoAnterior, alturaAnterior) / 100;
+	var imcNuevo = calcularIMC(pesoNuevo, alturaNuevo) / 100;
+
+	if(imcNuevo - imcViejo > 0.10 || imcNuevo - imcViejo < -0.10){
+		return true;
+	}
+	return false;
+}
+
+/**
+ pacienteIndex int
+
+ return undefined
+*/
+function consultaPaga(indexConsulta){
+	consultas[indexConsulta].consultaPaga = true;
+}
+
+/**
+ indexConsulta int
+
+ return undefined
+*/
+function finalizarConsulta(indexConsulta, descripcion){
+	consultasPendientes[indexConsulta].descripcion = descripcion;
+	consultasPendientes[indexConsulta].finalizada = true;
+	consultasFinalizadas
+		.push(consultasPendientes
+			.splice(indexConsulta, 1));
+}
+
+
+/**
+ habilita o deshabilita paciente
+ cedula int
+
+ return undefined
+*/
+function inhabilitarPaciente(cedula){
+	var indexPaciente = busquedaEnArray(paciente, "cedula", cedula);
+	paciente[indexPaciente].habilitado = true ? false : true;
+}
+
